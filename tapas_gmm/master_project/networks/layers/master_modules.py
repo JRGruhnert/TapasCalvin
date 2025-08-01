9604f8c291cbcb054b136fa96f0650b4c70e14b4
Jan Gruhnert <36522183+shurikan0@users.noreply.github.com>
2025-08-01 04:21:53 +0200

    Update master_modules.py

--- tapas_gmm/master_project/networks/layers/master_modules.py
+++ tapas_gmm/master_project/networks/layers/master_modules.py
@@ -46,7 +46,7 @@ class GinStandardMLP(nn.Sequential):
             hidden_dim = max(out_dim, in_dim // 2)
         super().__init__(
             nn.Linear(in_dim, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
+            nn.LayerNorm(hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, out_dim),
             nn.ReLU(),
@@ -57,7 +57,7 @@ class GinUnactivatedMLP(nn.Sequential):
     def __init__(self, dim_in: int):
         super().__init__(
             nn.Linear(dim_in, dim_in // 2),
-            nn.BatchNorm1d(dim_in // 2),
+            nn.LayerNorm(dim_in // 2),
             nn.ReLU(),
             nn.Linear(dim_in // 2, 1),
         )
